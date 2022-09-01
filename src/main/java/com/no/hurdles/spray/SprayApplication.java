8a4a0915da2de8caba5106a1cad9f782a8320d7e
User <1041579785@qq.com>
2022-09-01 18:20:04 +0800

    项目初始化

---
+++ src/main/java/com/no/hurdles/spray/SprayApplication.java
@@ -0,0 +1,12 @@
+package com.no.hurdles.spray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class SprayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprayApplication.class, args);
+    }
+
+}
