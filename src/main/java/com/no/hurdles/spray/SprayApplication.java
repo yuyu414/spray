7bc75f6a0f1b857e27590fd8c08946c7c1dc3aa4
User <1041579785@qq.com>
2022-09-07 17:18:58 +0800

    format

--- src/main/java/com/no/hurdles/spray/SprayApplication.java
+++ src/main/java/com/no/hurdles/spray/SprayApplication.java
@@ -3,7 +3,8 @@ package com.no.hurdles.spray;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication public class SprayApplication {
+@SpringBootApplication
+public class SprayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SprayApplication.class, args);
